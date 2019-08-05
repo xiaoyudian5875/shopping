f999cd34a8884ee47765e93790fab53c134f04aa
xiaoyudian5875 <1211996133@qq.com>
2019-08-05 14:16:04 +0800

    ww

--- src/main/java/com/hzit/shopping/bean/User.java
+++ src/main/java/com/hzit/shopping/bean/User.java
@@ -3,4 +3,9 @@ package com.hzit.shopping.bean;
 public class User {
     private  String  name ;
     private  int age;
+
+    public static void main(String[] args) {
+        float x=1,y=2,z=3;
+        System.out.println(y+=z--/++x);
+    }
 }
