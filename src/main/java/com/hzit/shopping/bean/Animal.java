2373d1359960c25ddd758a76ca115cc6d2dceb08
xiaoyudian5875 <1211996133@qq.com>
2019-08-07 15:23:28 +0800

    ww

---
+++ src/main/java/com/hzit/shopping/bean/Animal.java
@@ -0,0 +1,30 @@
+package com.hzit.shopping.bean;
+
+/**
+ * 定于一个父类
+ */
+public  class Animal {
+    public void move(){
+        System.out.println("动物可以移动");
+    }
+}
+class Dog extends Animal{
+    public void move(){
+        System.out.println("狗可以跑");
+    }
+    public void bark(){
+        System.out.println("狗可以吠叫");
+    }
+}
+class TestDog{
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        dog.move();
+        animal.move();
+       // dog.bark();
+
+    }
+}
+
+
