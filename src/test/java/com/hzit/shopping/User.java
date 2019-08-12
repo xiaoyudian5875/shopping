f23012c61b4b9a038a0bc12e5178e2fc14e73c0f
xiaoyudian5875 <52074651+xiaoyudian5875@users.noreply.github.com>
2019-08-12 09:40:24 +0800

    Update User.java

--- src/test/java/com/hzit/shopping/User.java
+++ src/test/java/com/hzit/shopping/User.java
@@ -7,7 +7,7 @@ public class User {
     public static void main(String[] args) {
         float x=1,y=2,z=3;
         System.out.println(y+=z--/++x);
-
+        int add=150
         int $gds=25;
         int n=4;
         System.out.println(((n>4)?99.9:9));
